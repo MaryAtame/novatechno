1a1135d755521e21f0f81204a43b4ed590d56f6b
MaryAtame <104096369+MaryAtame@users.noreply.github.com>
2022-09-14 07:26:08 +0200

    Add files via upload

--- src/js/modules/slick-slider.js
+++ src/js/modules/slick-slider.js
@@ -23,3 +23,28 @@ $(function () {
 });
 
 
+$(function () {
+	$(".prices-cards").slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 2,
+		responsive: [
+			{
+				breakpoint: 900,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 580,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+});
+
+
