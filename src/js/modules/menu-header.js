3cbd349bab4d823f136a8003168bd3e2bca7512d
MaryAtame <104096369+MaryAtame@users.noreply.github.com>
2022-10-11 10:41:25 +0200

    Update menu-header.js

--- src/js/modules/menu-header.js
+++ src/js/modules/menu-header.js
@@ -1,36 +1,59 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const menu = document.querySelectorAll('[data-menu]');
-	// menu.forEach((item) => {
-	// 	menuOpen(item);
-	// });
+
+
+
 });
 
-export function menuOpen() {
-	const anchors = document.querySelector('.menu-list__item');
-	const subMenuItems = document.getElementsByClassName('menu-sublist');
+const menuArray = document.querySelectorAll('[data-menu]');
 
-	anchors.forEach(function (item) {
-		item.addEventListener("click", function () {
-			let currentItem = item;
-			let subMenu = currentItem.getAttribute('data-dropdown');
+console.log(menuArray)
 
-			let subMenuOpen = subMenu.addEventListener("click", menuOpen);
-			let currentList = document.querySelector(subMenu);
-			if (anchors == subMenuItem) {
-				if (!currentList.classList.contains("menu-sublist--open")) {
+if (menuArray.length > 0) {
+	initMenu(menuArray);
+}
 
-					subMenu.forEach(function (item) {
-						item.classList.remove("menu-sublist--open");
-					});
+function initMenu(menuArray) {
+	menuArray.forEach(element => {
+		const menuItem = element.hasAttribute(['data-menu-item']);
+		console.log(menuItem)
 
-					subMenuOpen.forEach(function (item) {
-						item.classList.add("menu-sublist--open");
-					});
 
-					currentItem.classList.add("menu-sublist--open");
-					currentList.classList.add("menu-sublist--open");
-				}
-			}
-		});
 	});
 }
+
+// document.addEventListener('DOMContentLoaded', () => {
+// 	const menu = document.querySelectorAll('[data-menu]');
+// 	// menu.forEach((item) => {
+// 	// 	menuOpen(item);
+// 	// });
+// });
+
+// export function menuOpen() {
+// 	const anchors = document.querySelector('.menu-list__item');
+// 	const subMenuItems = document.getElementsByClassName('menu-sublist');
+
+// 	anchors.forEach(function (item) {
+// 		item.addEventListener("click", function () {
+// 			let currentItem = item;
+// 			let subMenu = currentItem.getAttribute('data-dropdown');
+
+// 			let subMenuOpen = subMenu.addEventListener("click", menuOpen);
+// 			let currentList = document.querySelector(subMenu);
+// 			if (anchors == subMenuItem) {
+// 				if (!currentList.classList.contains("menu-sublist--open")) {
+
+// 					subMenu.forEach(function (item) {
+// 						item.classList.remove("menu-sublist--open");
+// 					});
+
+// 					subMenuOpen.forEach(function (item) {
+// 						item.classList.add("menu-sublist--open");
+// 					});
+
+// 					currentItem.classList.add("menu-sublist--open");
+// 					currentList.classList.add("menu-sublist--open");
+// 				}
+// 			}
+// 		});
+// 	});
+// }
