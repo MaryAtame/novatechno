1b2ceb5f3108dcfbebe13e1e417c1f01974db71e
MaryAtame <104096369+MaryAtame@users.noreply.github.com>
2022-10-12 14:08:48 +0200

    Delete src/js directory

--- src/js/modules/right-side-cards-slider.js
+++
@@ -1,7 +0,0 @@
-import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
-export const cardsSlider = new Swiper(".swiper-portfolio", {
-	slidesPerView: 3,
-	spaceBetween: 30,
-	keyboard: true,
-	loop: true,
-});
--- src/js/modules/right-side-slider.js
+++
@@ -1,9 +0,0 @@
-import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
-
-export const creditsSlider = new Swiper(".credits-slider", {
-	slidesPerView: 4,
-	spaceBetween: 30,
-	mousewheel: true,
-	keyboard: true,
-	loop: true,
-});
