0da4a64c35d97d848f45d116b58e9fd2fd0184b6
MaryAtame <104096369+MaryAtame@users.noreply.github.com>
2022-10-18 15:31:12 +0200

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+import "./modules/sliders.js";
+import "./modules/fancybox.js";
+import "./modules/burger.js";
+import "./modules/menu-header.js";
+import "./modules/form-call.js";
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+});
